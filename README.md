cd0f51a8792c0b22e0b6c201e6af8c9d385d74e7
mike <mike.l.clifton@gmail.com>
2021-06-19 13:12:39 +0100

    Fixed asset loading

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Dart and Flutter Snippets
-Somewhere for me to put stuff when I'm trying to figure out how it works ... May be of use at a later date to when my memory lets me down!
+Somewhere for me to put stuff when I'm trying to figure out how it works ... may even be of use at a later date when my memory lets me down!
 
 Snippets are embedded in the tests - seemed like an easy way to have lots things I could run independently.
--- test/asset_load_test.dart
+++ test/asset_load_test.dart
@@ -3,6 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  // one to be aware of... without this line asset bundles don't load outside of widget tests
+  // See https://stackoverflow.com/questions/59027915/accessing-rootbundle-in-flutter-unit-test
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('asset loading snippets', () {
     test('I can load a file from the assets', () async {
       final theAsset = await rootBundle.loadString('assets/json_file_asset.json');
